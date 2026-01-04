21f92577155da1fc3e0b9570820db12231748990
Ramaraju413 <ramaraju086@gmail.com>
2026-01-04 22:42:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def get_user():
     
     return jsonify({
         'username': session['username'],
-        'authenticated': false
+        'authenticated': true
     }), 200
 
 @app.errorhandler(404)
